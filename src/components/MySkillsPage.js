83002d8f1fb817dcfe3a8d142029dbf759208f98
pranavrushi <thammenenipranav@gmail.com>
2023-08-03 08:25:36 +0530

    first commit

---
+++ src/components/MySkillsPage.js
@@ -0,0 +1,184 @@
+import React from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import {lightTheme} from './Themes';
+import { Develope} from './AllSvgs';
+
+
+import LogoComponent from '../subComponents/LogoComponent';
+import SocialIcons from '../subComponents/SocialIcons';
+import PowerButton from '../subComponents/PowerButton';
+import ParticleComponent from '../subComponents/ParticleComponent';
+import BigTitle from '../subComponents/BigTitlte'
+
+const Box = styled.div`
+background-color: ${props => props.theme.body};
+width: 100vw;
+height:100vh;
+position: relative;
+display: flex;
+justify-content: space-evenly;
+align-items: center;
+
+
+`
+
+const Main = styled.div`
+border: 2px solid ${props => props.theme.text};
+color: ${props => props.theme.text};
+background-color: ${props => props.theme.body};
+padding: 2rem;
+width: 70vw;
+height: 65vh;
+z-index:3;
+line-height: 1.5;
+cursor: pointer;
+
+font-family: 'Ubuntu Mono',monospace;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+
+&:hover{
+    color: ${props => props.theme.body};
+    background-color: ${props => props.theme.text};
+}
+`
+
+const Title = styled.h2`
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: calc(1em + 1vw);
+
+${Main}:hover &{
+    &>*{
+        fill:${props => props.theme.body};
+    }
+}
+
+&>*:first-child{
+margin-right: 1rem;
+}
+`
+
+const Description = styled.div`
+color: ${props => props.theme.text};
+font-size: calc(0.6em + 1vw);
+padding: 0.5rem 0;
+
+
+${Main}:hover &{
+   
+        color:${props => props.theme.body};
+    
+}
+
+strong{
+    margin-bottom: 1rem;
+    text-transform: uppercase;
+}
+ul,p{
+    margin-left: 2rem;
+}
+`
+
+const MySkillsPage = () => {
+    return (
+        <ThemeProvider theme={lightTheme}>
+<Box>
+
+<LogoComponent theme='light'/>
+<SocialIcons theme='light'/>
+<PowerButton />
+<ParticleComponent theme='light' />
+            {/* <Main> */}
+{/* <Title>
+    <Design width={40} height={40} /> Analytical Skills
+</Title>
+<Description>
+I am driven by a deep passion to tackle analytical problems head-on, leveraging my skills and insights to unravel complex challenges and pave the way for effective solutions.
+I am passionate about solving analytical problems, using my skills to unravel complexities and find effective solutions.
+I am passionate about solving analytical problems with precision.
+</Description>
+<Description>
+<strong>Skills</strong>
+
+<p>
+Critical thinking, Logical reasoning, Data analysis, Pattern recognition, Problem-solving, Attention to detail, Research abilities, Decision-making, Data interpretation, Strategic thinking.
+Data Analysis,Data Visualization, SQL, Python, Problem Solving, Web Scraping, API Management, Cloud Computing .
+</p>
+</Description>
+<Description>
+<strong>Tools</strong>
+<ul>
+    <li>
+       Figma
+    </li>
+    
+</ul>
+<p>
+    Excel, Tableau, PowerBi, AWS
+</p>
+</Description> */}
+
+            {/* </Main> */}
+            <Main>
+<Title>
+    <Develope width={40} height={40} /> My Skills
+</Title>
+<Description>
+I am passionate about solving analytical problems with precision and creating interesting websites. 
+</Description>
+<Description>
+<strong>Skills</strong>
+{/* <p>
+Data Analysis,Data Visualization, SQL, Python, Problem Solving, Web Scraping, API Management, Cloud Computing .
+</p> */}
+<ul>
+    <li>
+    Data Analysis, Data Visualization, SQL, Python, Problem Solving, Web Scraping, API Development, Cloud Computing , Cyber Security.
+    </li>
+    <li>
+    Frontend Development, Database Development,  HTML/CSS/JavaScript, React.js, Node.js, Bootstrap, Version Control, Continuous Integration/Deployment, SEO Management.
+    {/* Frontend Development, Backend Development, Database Management, HTML/CSS, JavaScript, React, Angular, Node.js, Express.js, Python, Django, Ruby, Ruby on Rails, PHP, Laravel, Database (SQL/NoSQL), API Development, Version Control (Git), UI/UX Design, Testing and Debugging, Problem-Solving, Agile Methodology, Project Management, Continuous Integration/Deployment. */}
+    </li>
+    
+</ul>
+</Description>
+<Description>
+<strong>Tools</strong>
+<p>
+    VScode, Python, Numpy, Pandas, Scikit-learn,, Excel, PowerBi, Tableau, SQL, GIT, AWS .
+</p>
+</Description>
+{/* <Title>
+    <Develope width={40} height={40} />  Developer
+</Title>
+<Description>
+I value business or brand for which i'm creating, thus i enjoy bringing new ideas to life.
+</Description>
+<Description>
+<strong>Skills</strong>
+<p>
+Html, Css, Js, React, Redux, Sass, Bootstrap, Tailwind, Firebase etc.
+</p>
+</Description>
+<Description>
+<strong>Tools</strong>
+<p>
+VScode, Github, Codepen etc.
+</p>
+</Description> */}
+
+            </Main>
+
+            <BigTitle text="SKILLS" top="80%" right="30%" />
+
+        </Box>
+
+        </ThemeProvider>
+        
+    )
+}
+
+export default MySkillsPage
---
+++ src/data/BlogData.js
@@ -0,0 +1,66 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"START WITH WHY",
+        tags:["Simon Sinek"],
+        // date:"13 May, 2021",
+        // imgSrc:"https://www.google.com/url?sa=i&url=https%3A%2F%2Famazingworkplaces.co%2Fstart-with-why-by-simon-sinek-taking-the-right-step-with-inspiration-from-successful-leaders%2F&psig=AOvVaw0edKao8gdw6UAMeTr8I0yU&ust=1691114208339000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNjejb6xv4ADFQAAAAAdAAAAABAE",
+        link:""
+    },
+    {
+        id:2,
+        name:"SURROUNDED BY IDIOTS",
+        tags:["Thomas Erikson"],
+        // date:"13 May, 2021",
+        // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:""
+    },
+    {
+        id:3,
+        name:"THE POWER OF SUB CONSCIOUS MIND",
+        tags:["Joseph Murphy"],
+        // date:"13 May, 2021",
+        // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:""
+    },
+    {
+        id:4,
+        name:"ATOMIC HABITS",
+        tags:["James Clear"],
+        // date:"13 May, 2021",
+        // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:""
+    },
+    {
+        
+    },
+    // {
+    //     id:6,
+    //     name:"How to implement Pagination Component in React from scratch",
+    //     tags:["react","css"],
+    //     date:"5 Jan, 2021",
+    //     imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625240050668%2FGAya9b0XK.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+    //     link:"https://codebucks.hashnode.dev/pagination-in-react"
+
+    // },
+   
+    // {
+    //     id:7,
+    //     name:"What is Redux ? Simply Explained!",
+    //     tags:["react","redux","javascript"],
+    //     date:"16 May, 2021",
+    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.png",
+    //     link:"https://dev.to/codebucks/what-is-redux-simply-explained-2ch7"
+    // },
+    // {
+    //     id:8,
+    //     name:"How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
+    //     tags:["react","framer-motion","styled-components"],
+    //     date:"26 May, 2021",
+    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+    //     link:"https://dev.to/codebucks/build-sidebar-navigation-menu-in-reactjs-with-react-router-and-framer-motion-for-cool-page-transition-effects-4bc3"
+    // },
+  
+] 
