9ca4d6b671ddf8697373d7d402edf3aa85bc4e7a
pranavrushi <thammenenipranav@gmail.com>
2023-08-07 15:38:47 +0530

    blogs page addition

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Portfolio - Pranav Rushi
+
+npm install
+npm start
+npm run deploy
--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -34,17 +34,22 @@ export const Blogs = [
         link:""
     },
     {
-        
+        id:5,
+        name:"THE SUBTLE ART OF NOT GIVING A FUCK",
+        tags:["MARK MANSON"],
+        // date:"13 May, 2021",
+        // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:""
     },
-    // {
-    //     id:6,
-    //     name:"How to implement Pagination Component in React from scratch",
-    //     tags:["react","css"],
-    //     date:"5 Jan, 2021",
-    //     imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625240050668%2FGAya9b0XK.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-    //     link:"https://codebucks.hashnode.dev/pagination-in-react"
+    {
+        id:6,
+        name:"HOW TO WIN FRIENDS & INFLUENCE PEOPLE",
+        tags:["DALE CARNEGIE"],
+        // date:"5 Jan, 2021",
+        // imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625240050668%2FGAya9b0XK.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+        link:""
 
-    // },
+    },
    
     // {
     //     id:7,
