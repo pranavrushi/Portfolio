bc00312b034571a56665c8a45658289bfeafc5ee
pranavrushi <thammenenipranav@gmail.com>
2023-08-08 14:02:33 +0530

    works page addition

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -2,62 +2,62 @@
 
 export const Work = [
     {
-        // id:1,
-        // name:"Agency Landing Page",
-        // description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
-        // tags:["react","gsap","styledComponents"],
+        id:1,
+        name:"Forecasting using ARIMA",
+        description:"Analysis of sales of Perrin Freres Champagne sales using ARIMA ",
+        tags:["Machine Learning","ARIMA"],
         
-        // demo:"https://agency-website-eta.vercel.app/",
-        // github:"https://github.com/codebucks27/Agency-website"
+        medium:"https://medium.com/@virinchithinks/the-arima-model-consists-of-three-main-components-ar-i-and-ma-let-me-explain-each-one-briefly-cc2e5aeedf2e",
+        github:"https://github.com/pranavrushi/ARIMA-Forecasting"
     },
-    {
-        // id:2,
-        // name:"Job listing website",
-        // description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
-        // tags:["react","sass"],
+    // {
+    //     id:2,
+    //     name:"",
+    //     description:"",
+    //     tags:[],
         
-        // demo:"http://react-job-listing-website.codebucks27.vercel.app/",
-        // github:"https://github.com/codebucks27/react-job-listing-website"
+    //     demo:"",
+    //     github:""
         
-    },
-    {
-        // id:3,
-        // name:"Dev.to Clone",
-        // description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
-        // tags:["react","api","sass"],
+    // },
+    // {
+    //     id:3,
+    //     name:"Dev.to Clone",
+    //     description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
+    //     tags:["react","api","sass"],
         
-        // demo:"http://devto-clone-codebucks27.vercel.app/",
-        // github:"https://github.com/codebucks27/devto-clone"
-    },
-    {
-        // id:4,
-        // name:"Sidebar Menu",
-        // description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
-        // tags:["react","styledComponents","react-router"],
+    //     demo:"http://devto-clone-codebucks27.vercel.app/",
+    //     github:"https://github.com/codebucks27/devto-clone"
+    // },
+    // {
+    //     id:4,
+    //     name:"Sidebar Menu",
+    //     description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+    //     tags:["react","styledComponents","react-router"],
         
-        // demo:"https://react-sidebar.vercel.app/",
-        // github:"https://github.com/codebucks27/react-sidebar"
+    //     demo:"https://react-sidebar.vercel.app/",
+    //     github:"https://github.com/codebucks27/react-sidebar"
        
-    },{
-        // id:5,
-        // name:"Todo App(React + Redux)",
-        // description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
-        // tags:["react","redux","framer-motion"],
+    // },{
+    //     // id:5,
+    //     // name:"Todo App(React + Redux)",
+    //     // description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+    //     // tags:["react","redux","framer-motion"],
         
-        // demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
-        // github:"https://github.com/codebucks27/React-Redux-Todo-App"
+    //     // demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+    //     // github:"https://github.com/codebucks27/React-Redux-Todo-App"
         
-    },
-    {
-        // id:6,
-        // name:"Responsive Navigation Menu",
-        // description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
-        // tags:["react","react-router","css"],
+    // },
+    // {
+    //     // id:6,
+    //     // name:"Responsive Navigation Menu",
+    //     // description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+    //     // tags:["react","react-router","css"],
         
-        // demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
-        // github:"https://github.com/codebucks27/React-responsive-navbar"
+    //     // demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
+    //     // github:"https://github.com/codebucks27/React-responsive-navbar"
         
-    },
+    // },
     
   
 ] 
